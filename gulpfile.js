c018e2e095df4e6eb5b218b2a9a90e05cb782598
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-09-16 19:33:28 +0300

    Began v5

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,114 @@
 (function(){
-    'use strict';
-    var gulp = require('gulp'),
-        connect = require('gulp-connect'),
-        open = require('gulp-open'),
-        less = require('gulp-less'),
-        jade = require('gulp-jade'),
-        path = require('path'),
-        jshint = require('gulp-jshint'),
-        stylish = require('jshint-stylish'),
-        fs = require('fs'),
-        paths = {
-            root: './',
-            css: './css',
-            js: './js',
-            src: './src',
-            jade: './src/jade',
-            less: './src/less',
-        },
-        pages = [
-            {
-                src: './src/jade/index/index.jade',
-                dest: './'
-            },
-            {
-                src: './src/jade/api/index.jade',
-                dest: './api/'
-            },
-            {
-                src: './src/jade/demos/index.jade',
-                dest: './demos/'
-            },
-            {
-                src: './src/jade/get-started/index.jade',
-                dest: './get-started/'
-            },
-        ],
-        styles = [
-            {
-                src: './src/less/main.less',
-                dest: './css/'
-            }
-        ];
-    // Jade Filter
-    var jadeNative = require('jade');
-    jadeNative.filters['code'] = function (text) {
-        return text
-        .replace( /</g, '&lt;'   )
-        .replace( />/g, '&gt;'   )
+  'use strict';
+  var gulp = require('gulp'),
+  connect = require('gulp-connect'),
+  open = require('gulp-open'),
+  less = require('gulp-less'),
+  pug = require('gulp-pug'),
+  path = require('path'),
+  paths = {
+    root: './',
+    css: './css',
+    js: './js',
+    src: './src',
+    pug: './src/pug',
+    less: './src/less',
+  },
+  pages = [
+    {
+      src: './src/pug/index/index.pug',
+      dest: './'
+    },
+    {
+      src: './src/pug/api/index.pug',
+      dest: './api/'
+    },
+    {
+      src: './src/pug/demos/index.pug',
+      dest: './demos/'
+    },
+    {
+      src: './src/pug/get-started/index.pug',
+      dest: './get-started/'
+    },
+  ],
+  styles = [
+    {
+      src: './src/less/main.less',
+      dest: './css/'
     }
-    /* ==================================================================
-    Build
-    ================================================================== */
-    gulp.task('less', function (cb) {
-        var cbs = 0;
-        styles.forEach(function (style) {
-            gulp.src([style.src])
-                .pipe(less({
-                    paths: [ path.join(__dirname, 'less', 'includes') ]
-                }))
-                .pipe(gulp.dest(style.dest))
-                .pipe(connect.reload())
-                .on('end', function () {
-                    cbs ++;
-                    if (cbs === styles.length) cb();
-                });
-        });
+  ];
+  // pug Filter
+  var pugNative = require('pug');
+  pugNative.filters['code'] = function (text) {
+    return text
+    .replace( /</g, '&lt;'   )
+    .replace( />/g, '&gt;'   )
+  }
+  /* ==================================================================
+  Build
+  ================================================================== */
+  gulp.task('less', function (cb) {
+    var cbs = 0;
+    return styles.forEach(function (style) {
+      gulp.src([style.src])
+      .pipe(less({
+        paths: [ path.join(__dirname, 'less', 'includes') ]
+      }))
+      .pipe(gulp.dest(style.dest))
+      .pipe(connect.reload())
+      .on('error', (err) => {
+        console.log(err);
+      })
+      .on('end', function () {
+        cbs ++;
+        if (cbs === styles.length) cb();
+      });
     });
-    gulp.task('jade', function (cb) {
-        var cbs = 0;
-        pages.forEach(function (page) {
-            gulp.src([page.src])
-                .pipe(jade({
-                    jade: jadeNative,
-                    pretty: true,
-                }))
-                .pipe(gulp.dest(page.dest))
-                .pipe(connect.reload())
-                .on('end', function () {
-                    cbs ++;
-                    if (cbs === pages.length) cb();
-                });
-        });
+  });
+  gulp.task('pug', function (cb) {
+    var cbs = 0;
+    pages.forEach(function (page) {
+      gulp.src([page.src])
+      .pipe(pug({
+        pug: pugNative,
+        pretty: true,
+      }))
+      .pipe(gulp.dest(page.dest))
+      .pipe(connect.reload())
+      .on('end', function () {
+        cbs ++;
+        if (cbs === pages.length) cb();
+      });
     });
+  });
 
-    gulp.task('build', ['jade', 'less'], function (cb) {
-        cb();
+  gulp.task('build', gulp.series('pug', 'less'), function (cb) {
+    cb();
+  });
+
+  /* =================================
+  Watch
+  ================================= */
+  gulp.task('watch', function () {
+    gulp.watch(paths.less + '**/*.*', gulp.series([ 'less' ]));
+    gulp.watch('./src/pug/**/*.pug', gulp.series([ 'pug' ]));
+  });
+
+  gulp.task('connect', function () {
+    return connect.server({
+      root: [ paths.root ],
+      livereload: true,
+      port:'3000'
     });
+  });
+  gulp.task('open', function () {
+    return gulp.src('./index.html').pipe(open({ uri: 'http://localhost:3000/index.html'}));
+  });
 
-    /* =================================
-    Watch
-    ================================= */
-    gulp.task('watch', function () {
-        gulp.watch(paths.less + '**/*.*', [ 'less' ]);
-        gulp.watch(paths.jade + '**/**/*.*', [ 'jade' ]);
-    });
+  gulp.task('server', gulp.parallel([ 'watch', 'connect', 'open' ]));
 
-    gulp.task('connect', function () {
-        return connect.server({
-            root: [ paths.root ],
-            livereload: true,
-            port:'3000'
-        });
-    });
-    gulp.task('open', function () {
-        return gulp.src('./index.html').pipe(open({ uri: 'http://localhost:3000/index.html'}));
-    });
+  gulp.task('default', gulp.series([ 'server' ]));
 
-    gulp.task('server', [ 'watch', 'connect', 'open' ]);
-
-    gulp.task('default', [ 'server' ]);
-
-    gulp.task('test', [ 'build' ]);
+  gulp.task('test', gulp.series([ 'build' ]));
 })();
--- js/main.js
+++ js/main.js
@@ -1,155 +1,109 @@
 ;(function(){
-    function lazyLoadImages() {
-        var st = $(window).scrollTop();
-        $('img.lazy').each(function(){
-            var img = $(this);
-            if (img.attr('src')) return;
-            if (img.offset().top < $(window).height() + st) {
-                img.attr('src', img.data('src'));
-            }
-        });
-    }
-    if ($('img.lazy').length > 0) {
-        $(window).scroll(function(){
-            lazyLoadImages();
-        });
-    }
-    if (window.hljs) {
-        hljs.configure({tabReplace: '    '});
-        hljs.initHighlightingOnLoad();
-    }
+  function lazyLoadImages() {
+    var st = $(window).scrollTop();
+    $('img.lazy').each(function(){
+      var img = $(this);
+      if (img.attr('src')) return;
+      if (img.offset().top < $(window).height() + st) {
+        img.attr('src', img.data('src'));
+      }
+    });
+  }
+  if ($('img.lazy').length > 0) {
+    $(window).scroll(function(){
+      lazyLoadImages();
+    });
+  }
+  if (window.hljs) {
+    hljs.configure({tabReplace: '    '});
+    hljs.initHighlightingOnLoad();
+  }
 
-    if ($('.header-swiper-front').length) {
-      // Header swipers
-      var swiperBack2 = new Swiper('.header-swiper-back-2', {
-          slidesPerView: 'auto',
-          centeredSlides: true,
-          spaceBetween: 100,
-          onlyExternal: true,
-          effect: 'coverflow',
-          direction: 'vertical',
-          speed: 600,
-          coverflowEffect: {
-              slideShadows: false
+  if ($('.header-swiper-front').length) {
+
+    var swiperFront = new Swiper('.header-swiper-front', {
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      spaceBetween: 100,
+      effect: 'coverflow',
+      speed: 600,
+      coverflowEffect: {
+        slideShadows: true
+      },
+      pagination: {
+        el: '.header-swiper-front .swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.header-swiper-front .swiper-button-next',
+        prevEl: '.header-swiper-front .swiper-button-prev',
+      },
+      keyboard: true,
+      a11y: true,
+      on: {
+        slideChange: function () {
+          var s = this;
+          if (s.activeIndex === $('.swiper-slide-gallery').index()) {
+            $(s.el).find('.swiper-pagination').hide();
           }
-      });
-      var swiperBack1 = new Swiper('.header-swiper-back-1', {
-          slidesPerView: 'auto',
-          centeredSlides: true,
-          spaceBetween: 300,
-          effect: 'coverflow',
-          speed: 600,
-          coverflowEffect: {
-              slideShadows: false
+          else {
+            $(s.el).find('.swiper-pagination').show();
           }
-      });
-      
-      var swiperFront = new Swiper('.header-swiper-front', {
-          slidesPerView: 'auto',
-          centeredSlides: true,
-          spaceBetween: 100,
-          effect: 'coverflow',
-          speed: 600,
-          coverflowEffect: {
-              slideShadows: false
-          },
-          pagination: {
-              el: '.header-swiper-front .swiper-pagination',
-              clickable: true,
-          },
-          navigation: {
-              nextEl: '.header-swiper-front .swiper-button-next',
-              prevEl: '.header-swiper-front .swiper-button-prev',
-          },
-          controller: {
-              control: [swiperBack1, swiperBack2],
-              by: 'container',
-          },
-          keyboard: true,
-          a11y: true,
-          on: {
-            slideChange: function () {
-              var s = this;
-              if (s.activeIndex === $('.swiper-slide-gallery').index()) {
-                  $(s.el).find('.swiper-pagination').hide();
-              }
-              else {
-                  $(s.el).find('.swiper-pagination').show();
-              }
-            }
-          }
-      });
+        }
+      }
+    });
 
+    var galleryTopSwiper = new Swiper('.swiper-gallery-top', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      nested:true,
+      resistanceRatio: 0,
+      preloadImages: false,
+      lazy: true,
+    });
+    var galleryThumbsSwiper = new Swiper('.swiper-gallery-thumbs', {
+      slidesPerView: 5,
+      spaceBetween: 10,
+      centeredSlides: true,
+      touchRatio: 0.2,
+      slideToClickedSlide: true,
+      nested:true,
+      resistanceRatio: 0,
+    });
+    galleryTopSwiper.controller.control = galleryThumbsSwiper;
+    galleryThumbsSwiper.controller.control = galleryTopSwiper;
+  }
 
-      var galleryTopSwiper = new Swiper('.swiper-gallery-top', {
-          slidesPerView: 1,
-          spaceBetween: 10,
-          nested:true,
-          resistanceRatio: 0,
-          preloadImages: false,
-          lazy: true,
-      });
-      var galleryThumbsSwiper = new Swiper('.swiper-gallery-thumbs', {
-          slidesPerView: 5,
-          spaceBetween: 10,
-          centeredSlides: true,
-          touchRatio: 0.2,
-          slideToClickedSlide: true,
-          nested:true,
-          resistanceRatio: 0,
-      });
-      galleryTopSwiper.controller.control = galleryThumbsSwiper;
-      galleryThumbsSwiper.controller.control = galleryTopSwiper;
-    }
-
-    // FB
-    $('body').prepend('<div id="fb-root"></div>');
-    (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=129338113911206";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    // TW
-    (function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}})(document, 'script', 'twitter-wjs');
-    // G+
-    (function() {
-        var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-        po.src = 'https://apis.google.com/js/platform.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-    })();
-
-    // Demos
-    var iframeTimeout;
-    function lazyLoadDemos() {
-        $('.demo').each(function () {
-            var frameHolder = $(this).find('.demo-iframe');
-            if (frameHolder.hasClass('loaded')) return;
-            var frame = frameHolder.find('iframe');
-            var src = frame.attr('data-src');
-            if (frameHolder.offset().top + frameHolder.height() < $(window).scrollTop()) {
-              return;
-            }
-            if (frameHolder.offset().top < ($(window).scrollTop() + $(window).height() + 50)) {
-                frameHolder.addClass('loaded');
-                frame.attr('src', src);
-            }
-        });
-    }
-    if ($('.demo').length > 0) {
-        $('.demo').each(function () {
-            var t = $(this);
-            var demoFile = t.find('iframe').attr('data-src');
-            t.find('.demo-link-new-window').attr('href', demoFile);
-            t.find('.demo-link-source').attr('href', 'https://github.com/nolimits4web/Swiper/blob/master/demos/' + demoFile);
-        });
-        $(window).on('scroll resize', function () {
-            clearTimeout(iframeTimeout);
-            iframeTimeout = setTimeout(function () {
-              lazyLoadDemos();
-            }, 500);
-        });
+  // Demos
+  var iframeTimeout;
+  function lazyLoadDemos() {
+    $('.demo').each(function () {
+      var frameHolder = $(this).find('.demo-iframe');
+      if (frameHolder.hasClass('loaded')) return;
+      var frame = frameHolder.find('iframe');
+      var src = frame.attr('data-src');
+      if (frameHolder.offset().top + frameHolder.height() < $(window).scrollTop()) {
+        return;
+      }
+      if (frameHolder.offset().top < ($(window).scrollTop() + $(window).height() + 50)) {
+        frameHolder.addClass('loaded');
+        frame.attr('src', src);
+      }
+    });
+  }
+  if ($('.demo').length > 0) {
+    $('.demo').each(function () {
+      var t = $(this);
+      var demoFile = t.find('iframe').attr('data-src');
+      t.find('.demo-link-new-window').attr('href', demoFile);
+      t.find('.demo-link-source').attr('href', 'https://github.com/nolimits4web/Swiper/blob/master/demos/' + demoFile);
+    });
+    $(window).on('scroll resize', function () {
+      clearTimeout(iframeTimeout);
+      iframeTimeout = setTimeout(function () {
         lazyLoadDemos();
-    }
+      }, 500);
+    });
+    lazyLoadDemos();
+  }
 })();
