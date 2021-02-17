db326f77f7fc375464c4a3979421a6580acbf431
Vladimir Kharlampidi <nolimits4web@gmail.com>
2021-02-17 13:50:41 +0300

    Update track-outbound.js

--- src/shared/track-outbound.js
+++ src/shared/track-outbound.js
@@ -1,5 +1,8 @@
 export const trackOutbound = (url) => {
-  if (typeof window !== 'undefined' && window.ga && url) {
-    window.ga('send', 'event', 'outbound', 'click', url);
+  if (typeof window !== 'undefined' && window.gtag && url) {
+    window.gtag('event', 'click', {
+      event_category: 'outbound',
+      event_label: url,
+    });
   }
 };
