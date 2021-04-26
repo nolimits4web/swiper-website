386ecb31b7c2fcc1407cbd9294708966e4ef86ef
Vlad <vl.tansky@gmail.com>
2021-04-26 09:36:04 +0300

    feat: demos

--- demos/scripts/index.js
+++ demos/scripts/index.js
@@ -1,16 +1,18 @@
 const fs = require('fs-extra');
 const path = require('path');
 const globby = require('globby');
+const elapsed = require('elapsed-time-logger');
 const buildStatic = require('./static');
 const buildAngular = require('./angular');
 const buildReact = require('./react');
 
 (async () => {
+  elapsed.start('Demos generation');
   const demos = await globby(['src/*', '!src/default_settings.js'], {
     cwd: path.join(__dirname, '/..'),
   });
   const demosData = [];
-  demos.forEach(async (item) => {
+  await demos.map(async (item) => {
     try {
       const folderName = path.basename(item.replace(/\.[^/.]+$/, ''));
       const demoConfig = require(path.join(__dirname, '../', item));
@@ -36,4 +38,5 @@ const buildReact = require('./react');
 
   const demosJSON_path = path.join(__dirname, `../../src/demos.json`);
   await fs.writeFile(demosJSON_path, JSON.stringify(demosData, null, 2));
+  elapsed.end('Demos generation');
 })();
