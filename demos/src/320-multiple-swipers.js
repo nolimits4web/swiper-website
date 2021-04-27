c638623250007e0703700fd289b6b5ecb3e2f594
Vlad <vl.tansky@gmail.com>
2021-04-27 12:37:12 +0300

    fix(demos): multiple swipers

--- demos/src/320-multiple-swipers.js
+++ demos/src/320-multiple-swipers.js
@@ -3,6 +3,19 @@ const defaultSettings = require('./default_settings');
 module.exports = (mode = 'core') => ({
   ...defaultSettings,
   title: 'Multiple swipers',
+  styles: `
+  ${defaultSettings.styles}
+
+  body {
+    background: #eee;
+  }
+
+  .swiper-container {
+    width: 100%;
+    height: 300px;
+    margin: 20px 0;
+  }
+  `,
   content: `<Swiper>
   ${Array.from({ length: 9 })
     .map((el, index) => `<SwiperSlide>Slide ${index + 1}</SwiperSlide>`)
