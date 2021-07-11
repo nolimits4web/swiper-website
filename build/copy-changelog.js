622351cb5820626d19b3ad011d04f4f6ee0db55c
Vlad Tansky <vl.tansky@gmail.com>
2021-07-11 13:57:28 +0300

    feat: changelog (#161)

---
+++ build/copy-changelog.js
@@ -0,0 +1,30 @@
+const fs = require('fs-extra');
+const fetch = require('node-fetch');
+const banner =
+  `---
+title: Swiper Changelog
+description: Swiper changelog.
+---` + '\n';
+
+async function writeFile(content) {
+  await fs.writeFile(
+    './src/pages/changelog.mdx',
+    banner + content.replace(/\# \[/g, '## [')
+  );
+  console.log('copy changelog done');
+}
+
+(async () => {
+  const localPath = '../swiper/CHANGELOG.md';
+  const exists = fs.existsSync(localPath);
+  if (exists) {
+    const content = await fs.readFile(localPath, 'utf-8');
+    await writeFile(content);
+  }
+  console.log(`local didnt find: ${localPath}. Fetching from web`);
+  const response = await fetch(
+    'https://raw.githubusercontent.com/nolimits4web/swiper/master/CHANGELOG.md'
+  );
+  const content = await response.text();
+  await writeFile(content);
+})();
