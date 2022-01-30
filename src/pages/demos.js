7356af4ddaa83b6319b58c0b0f11bf57cc1936aa
Vlad Tansky <vl.tansky@gmail.com>
2022-01-30 23:16:07 +0200

    feat(demos): source code tabs

--- src/pages/demos.js
+++ src/pages/demos.js
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, Fragment, useEffect } from 'react';
 import { ReactComponent as CodeSandBoxLogo } from '@/img/codesandbox.svg';
+import { ReactComponent as CodeIcon } from '@/img/code.svg';
 import Heading from '@/components/Heading';
 import { WithSidebarLayout } from '@/layouts/withSidebar';
 import { useLazyDemos } from 'src/shared/use-lazy-demos';
@@ -9,8 +10,184 @@ import codeSandboxFiles from 'src/shared/codesandbox/codesandbox-files';
 import { compressToBase64 } from 'src/shared/lz-string';
 import Carbon from '@/components/Carbon';
 import { trackOutbound } from 'src/shared/track-outbound';
+import { Listbox, Transition, Tab } from '@headlessui/react';
+import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+import clsx from 'clsx';
 
 let tableOfContents;
+
+const langList = ['Core', 'React', 'Vue', 'Angular', 'Svelte'];
+
+async function getDemoContent(folder, mode) {
+  const fileName = {
+    angular: 'angular.json',
+    core: 'core.html',
+    react: 'react.json',
+    svelte: 'svelte.json',
+    vue: 'vue.json',
+  }[mode.toLowerCase()];
+  const _mainContent = await fetch(`demos/${folder}/${fileName}`);
+  if (mode === 'core') {
+    let mainContent = await _mainContent.text();
+    return {
+      'index.html': {
+        content: mainContent,
+      },
+    };
+  }
+  return _mainContent.json();
+}
+
+function LanguageSwitcher({ value, onChange }) {
+  return (
+    <div className="w-72">
+      <Listbox value={value} onChange={onChange}>
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+            <span className="block truncate">{value}</span>
+            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+              <SelectorIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              {langList.map((lang) => (
+                <Listbox.Option
+                  key={lang}
+                  className={({ active }) =>
+                    `${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'}
+                      relative cursor-default select-none py-2 pl-10 pr-4`
+                  }
+                  value={lang}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`${
+                          selected ? 'font-medium' : 'font-normal'
+                        } block truncate`}
+                      >
+                        {lang}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`${
+                            active ? 'text-amber-600' : 'text-amber-600'
+                          }
+                            absolute inset-y-0 left-0 flex items-center pl-3`}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
+      </Listbox>
+    </div>
+  );
+}
+
+function DemoItem({ title, slug, folder, skip, openCodeSandbox }) {
+  const [currentLang, setCurrentLang] = useState('react');
+  const [open, setOpen] = useState(false);
+  const [content, setContent] = useState({});
+
+  useEffect(() => {
+    if (open) {
+      getDemoContent(folder, currentLang).then((value) => setContent(value));
+    }
+  }, [open, currentLang]);
+
+  return (
+    <>
+      <Heading level={2} id={slug} toc={true}>
+        {title}
+      </Heading>
+      <div className="my-4 flex flex-wrap text-sm">
+        <a
+          className="mb-2 mr-4 no-underline"
+          href={`/demos/${folder}/core.html`}
+          target="_blank"
+          rel="noopener"
+        >
+          Open in new window
+        </a>
+      </div>
+      <div className="demo mt-4 bg-gray-100 shadow">
+        <iframe
+          data-src={`/demos/${folder}/core.html`}
+          scrolling="no"
+          frameBorder="0"
+          className="block h-96 w-full"
+        ></iframe>
+      </div>
+      <div className="flex">
+        <LanguageSwitcher value={currentLang} onChange={setCurrentLang} />
+        <div className="flex flex-grow items-center justify-end">
+          <button
+            type="button"
+            className="inline-flex items-center"
+            onClick={() => setOpen((val) => !val)}
+          >
+            <CodeIcon className="mr-1" /> source code
+          </button>
+          {!skip?.includes(currentLang.toLowerCase()) && (
+            <button
+              type="button"
+              className="ml-3 inline-flex items-center"
+              onClick={(e) => openCodeSandbox(e, title, folder, currentLang)}
+            >
+              <CodeSandBoxLogo className="mr-1" /> codesandbox
+            </button>
+          )}
+        </div>
+      </div>
+      {open && (
+        <div className="">
+          <Tab.Group>
+            <Tab.List className="flex space-x-1 rounded-xl bg-primary p-1">
+              {Object.keys(content).map((item) => (
+                <Tab
+                  key={item}
+                  className={({ selected }) =>
+                    clsx(
+                      'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
+                      'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                      selected
+                        ? 'bg-white shadow'
+                        : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                    )
+                  }
+                >
+                  {item}
+                </Tab>
+              ))}
+            </Tab.List>
+            <Tab.Panels>
+              {Object.values(content).map((item, itemIdx) => (
+                <Tab.Panel key={itemIdx}>
+                  <code>{item.content}</code>
+                </Tab.Panel>
+              ))}
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      )}
+    </>
+  );
+}
 
 export default function DemosPage() {
   tableOfContents = demos.map(({ title, slug }) => {
@@ -66,26 +243,6 @@ export default function DemosPage() {
     };
   };
 
-  async function getDemoContent(folder, mode) {
-    const path = {
-      angular: 'angular.json',
-      core: 'core.html',
-      react: 'react.json',
-      svelte: 'svelte.json',
-      vue: 'vue.json',
-    };
-    const _mainContent = await fetch(`demos/${folder}/${path[mode]}`);
-    if (mode === 'core') {
-      let mainContent = await _mainContent.text();
-      return {
-        'index.html': {
-          content: mainContent,
-        },
-      };
-    }
-    return _mainContent.json();
-  }
-
   const compressParameters = (parameters) => {
     return compressToBase64(JSON.stringify(parameters))
       .replace(/\+/g, `-`) // Convert '+' to '-'
@@ -95,6 +252,7 @@ export default function DemosPage() {
 
   const openCodeSandbox = async (e, title, folder, mode = 'core') => {
     e.preventDefault();
+    mode = mode.toLowerCase();
     const content = await getDemoContent(folder, mode);
     const codeSandBoxParams = compressParameters(
       generateCodeSandboxWorkspace(mode, content, title)
@@ -172,52 +330,12 @@ export default function DemosPage() {
           </div>
         ))}
       </div>
-      {demos.map(({ title, slug, folder, skip }, demoIndex) => (
-        <React.Fragment key={title}>
-          <Heading level={2} id={slug} toc={true}>
-            {title}
-          </Heading>
-          <div className="my-4 flex flex-wrap text-sm">
-            <a
-              className="mb-2 mr-4 no-underline"
-              href={`/demos/${folder}/core.html`}
-              target="_blank"
-              rel="noopener"
-            >
-              Open in new window
-            </a>
-            {['Core', 'React', 'Vue', 'Angular', 'Svelte'].map((name) => {
-              if (skip && skip.includes(name.toLowerCase())) {
-                return null;
-              }
-              return (
-                <a
-                  key={name}
-                  className="ml-2 no-underline"
-                  href="#"
-                  onClick={(e) =>
-                    openCodeSandbox(e, title, folder, `${name.toLowerCase()}`)
-                  }
-                >
-                  <CodeSandBoxLogo
-                    className="inline fill-current"
-                    width="19"
-                    height="14"
-                  />
-                  {name}
-                </a>
-              );
-            })}
-          </div>
-          <div className="demo my-4 bg-gray-100 shadow">
-            <iframe
-              data-src={`/demos/${folder}/core.html`}
-              scrolling="no"
-              frameBorder="0"
-              className="block h-96 w-full"
-            ></iframe>
-          </div>
-        </React.Fragment>
+      {demos.map((demo, demoIndex) => (
+        <DemoItem
+          {...demo}
+          key={demo.title}
+          openCodeSandbox={openCodeSandbox}
+        />
       ))}
     </WithSidebarLayout>
   );
