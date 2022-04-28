29547a5acdc008bb1df0e8ac0c875a152d9309a6
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-04-28 18:26:45 +0300

    chore: update home projects appearance

--- src/components/HomeProjects.js
+++ src/components/HomeProjects.js
@@ -3,111 +3,48 @@ import { Atropos } from 'atropos/react';
 
 export default function HomeProjects() {
   return (
-    <div className="space-y-8 sm:flex sm:space-y-0 sm:space-x-8">
-      <Atropos
-        component="a"
-        highlight
-        href="https://framework7.io"
-        target="_blank"
-        className="project-banner project-banner-f7 mx-auto inline-block sm:block"
-        innerClassName="border border-black border-opacity-10 hover:border-opacity-0"
-        rotateTouch="scroll-y"
-        onClick={() => trackOutbound('https://framework7.io')}
-      >
-        <img
-          className="project-banner-spacer"
-          src="/images/projects/f7-frame.svg"
-          alt="framework7 logo"
-        />
-        <img
-          data-atropos-offset="-2.5"
-          src="/images/projects/f7-frame.svg"
-          alt="framework7 logo"
-        />
-        <img
-          data-atropos-offset="0"
-          src="/images/projects/f7-logo.svg"
-          alt="framework7 logo"
-        />
-        <img
-          data-atropos-offset="5"
-          src="/images/projects/f7-work.svg"
-          alt="framework7 logo"
-        />
-      </Atropos>
-      <Atropos
-        component="a"
-        highlight
-        href="https://konstaui.com"
-        target="_blank"
-        className="project-banner project-banner-twm mx-auto inline-block sm:block"
-        onClick={() => trackOutbound('https://konstaui.com')}
-        innerClassName="border border-black border-opacity-10 hover:border-opacity-0"
-        rotateTouch="scroll-y"
-      >
-        <img
-          className="project-banner-spacer"
-          src="/images/projects/twm-text.svg"
-          alt="konsta-logo"
-        />
-        <img
-          data-atropos-offset="0"
-          src="/images/projects/twm-text.svg"
-          alt="konsta-logo"
-        />
-        <img
-          data-atropos-offset="2.5"
-          src="/images/projects/twm-logo-back.svg"
-          alt="konsta-logo"
-        />
-        <img
-          data-atropos-offset="5"
-          src="/images/projects/twm-logo-top.svg"
-          alt="konsta-logo"
-        />
-        <img
-          data-atropos-offset="7.5"
-          src="/images/projects/twm-logo-bottom.svg"
-          alt="konsta-logo"
-        />
-      </Atropos>
-      <Atropos
-        component="a"
-        highlight
-        href="https://atroposjs.com"
-        target="_blank"
-        className="project-banner project-banner-atropos mx-auto inline-block sm:block"
-        innerClassName="border border-black border-opacity-10 hover:border-opacity-0"
-        rotateTouch="scroll-y"
-        onClick={() => trackOutbound('https://atroposjs.com')}
-      >
-        <img
-          className="project-banner-spacer"
-          src="/images/projects/atropos-text.svg"
-          alt="atropos-logo"
-        />
-        <img
-          data-atropos-offset="0"
-          src="/images/projects/atropos-text.svg"
-          alt="atropos-logo"
-        />
-        <img
-          data-atropos-offset="3"
-          src="/images/projects/atropos-logo-back.svg"
-          alt="atropos-logo"
-        />
-        <img
-          data-atropos-offset="6"
-          src="/images/projects/atropos-logo-front.svg"
-          alt="atropos-logo"
-        />
-        <span
-          data-atropos-offset="25"
-          className="project-banner-badge absolute right-4 top-4 z-10 rounded-full bg-green-500 py-1 px-2 text-xs font-semibold text-white"
+    <div className="gr mx-auto max-w-3xl items-stretch space-y-4 sm:flex sm:space-y-0 sm:space-x-8 sm:text-center">
+      {[
+        {
+          url: 'https://framework7.io',
+          title: 'Framework7',
+          description:
+            'Full featured framework for building iOS, Android & desktop apps',
+          logo: 'framework7.svg',
+        },
+        {
+          url: 'https://atroposjs.com',
+          title: 'Atropos',
+          description: 'Stunning touch-friendly 3D parallax hover effects',
+          logo: 'atropos.svg',
+        },
+        {
+          url: 'https://konstaui.com',
+          title: 'Konsta UI',
+          description:
+            'Pixel perfect mobile UI components built with Tailwind CSS',
+          logo: 'konsta.svg',
+        },
+      ].map((item) => (
+        <a
+          className="flex w-full items-center rounded-xl border border-black border-opacity-10 px-4 py-6 text-black duration-200 hover:border-opacity-0 hover:no-underline hover:shadow-lg sm:flex-col sm:hover:shadow-2xl"
+          href={item.url}
+          target="_blank"
+          key={item.title}
         >
-          NEW
-        </span>
-      </Atropos>
+          <img
+            className="mr-4 w-12 sm:mr-0 sm:h-32 sm:w-32"
+            src={`/images/projects/${item.logo}`}
+            alt={item.title}
+          />
+          <div>
+            <div className="font-semibold text-black sm:mt-4 sm:mb-2">
+              {item.title}
+            </div>
+            <div className="text-sm opacity-75">{item.description}</div>
+          </div>
+        </a>
+      ))}
     </div>
   );
 }
