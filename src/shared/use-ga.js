069c924a2dfc7654e4c12faadfba59c8204ba168
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-06-20 17:17:49 +0300

    fix: ga

--- src/shared/use-ga.js
+++ src/shared/use-ga.js
@@ -3,14 +3,14 @@ import { useEffect } from 'react';
 export const useGA = (router) => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-W7Y1DZYRT7';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-149857145-1';
     script.onload = () => {
       window.dataLayer = window.dataLayer || [];
       window.gtag = function () {
         dataLayer.push(arguments);
       };
       window.gtag('js', new Date());
-      window.gtag('config', 'G-W7Y1DZYRT7');
+      window.gtag('config', 'UA-149857145-1');
     };
     document.body.appendChild(script);
 
