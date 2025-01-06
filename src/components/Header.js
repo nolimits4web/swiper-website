715b5b222d50b72b834908795ec7efd97567d3cb
Vladimir Kharlampidi <nolimits4web@gmail.com>
2025-01-06 08:36:36 -0500

    chore: remove sale banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import Link from 'next/link';
 import { useRef, useState, useEffect } from 'react';
 import GithubStats from './GithubStats';
 import { useIsomorphicLayoutEffect } from '@/hooks/useIsomorphicLayoutEffect';
-import NewYearRibbon from './NewYearRibbon';
-// import TogglesRibbon from './TogglesRibbon';
+import TogglesRibbon from './TogglesRibbon';
 
 function updateColorTheme() {
   if (
@@ -75,7 +74,7 @@ export default function Header() {
 
   return (
     <>
-      <NewYearRibbon />
+      <TogglesRibbon />
       <header className="sticky top-0 z-50 h-16 bg-surface-2 py-6">
         <div className="mx-auto flex h-full items-center justify-between px-4 sm:px-6">
           {/* Left */}
