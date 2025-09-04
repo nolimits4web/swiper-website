353e03939d2c228147b66c42904139ae476822dc
Vladimir Kharlampidi <nolimits4web@gmail.com>
2025-09-04 11:06:33 -0400

    chore: add tinyadz

--- src/layouts/WithSidebarLayout.js
+++ src/layouts/WithSidebarLayout.js
@@ -97,6 +97,7 @@ export function WithSidebarLayout({
           {toc.length > 0 && (
             <TableOfContents onClick={onListClick} tableOfContents={toc} />
           )}
+          <div id="TA_AD_CONTAINER" className="-mx-4 -mb-4 mt-auto shrink-0" />
         </div>
       </div>
       {/* Center */}
