2c879c9b9d82274f8ce3350241807bcbdd6f764d
Vladimir Kharlampidi <nolimits4web@gmail.com>
2025-11-17 14:57:28 -0500

    chore: black friday banner

--- src/components/BlackFridayRibbon.js
+++ src/components/BlackFridayRibbon.js
@@ -1,10 +1,10 @@
 export default function BlackFridayRibbon() {
   return (
-    <div className="flex min-h-[64px] items-center  justify-center bg-surface-2 px-4 py-2 text-sm font-semibold  sm:px-6 lg:px-8 xl:px-10">
+    <div className="flex min-h-[54px] items-center  justify-center bg-surface-1 px-4 py-2 text-sm font-medium sm:px-6 lg:px-8 xl:px-10 border border-outline backdrop-blur-lg backdrop-saturate-200 rounded-full">
       <div className="text-center">
         🔥 Black Friday Sale: Up to 40% OFF on{' '}
         <a
-          className="underline hover:no-underline "
+          className="!underline hover:!no-underline !text-[inherit]"
           href="https://uiinitiative.com"
           target="_blank"
         >
@@ -12,15 +12,23 @@ export default function BlackFridayRibbon() {
         </a>
         ,{' '}
         <a
-          className="underline hover:no-underline"
+          className="!underline hover:!no-underline !text-[inherit]"
           href="https://studio.swiperjs.com"
           target="_blank"
         >
           Swiper Studio
+        </a>
+        ,{' '}
+        <a
+          className="!underline hover:!no-underline !text-[inherit]"
+          href="https://paneflow.com"
+          target="_blank"
+        >
+          PaneFlow
         </a>{' '}
         and{' '}
         <a
-          className="underline hover:no-underline"
+          className="!underline hover:!no-underline !text-[inherit]"
           href="https://t0ggles.com"
           target="_blank"
         >
