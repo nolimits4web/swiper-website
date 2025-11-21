e77addf91a913e9d8afcf40457638da1bea81093
Vladimir Kharlampidi <nolimits4web@gmail.com>
2025-11-21 11:19:06 -0500

    Update BlackFridayRibbon.js

--- src/components/BlackFridayRibbon.js
+++ src/components/BlackFridayRibbon.js
@@ -2,7 +2,7 @@ export default function BlackFridayRibbon() {
   return (
     <div className="flex min-h-[54px] items-center  justify-center bg-surface-1 px-4 py-2 text-sm font-medium sm:px-6 lg:px-8 xl:px-10 border border-outline backdrop-blur-lg backdrop-saturate-200 rounded-full">
       <div className="text-center">
-        🔥 Black Friday Sale: Up to 40% OFF on{' '}
+        🔥 Black Friday Sale: Up to 50% OFF on{' '}
         <a
           className="!underline hover:!no-underline !text-[inherit]"
           href="https://uiinitiative.com"
